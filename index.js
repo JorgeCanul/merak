21cc75420489b5b31f47ac190f53de5e8a89e14a
Jorge C <jorgec@Jorges-MacBook-Pro-2.local>
2020-09-09 21:53:15 -0700

    modified html, js, css

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-// add padding top to show content behind navbar
-// $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
-
-// detect scroll top or down
-if ($('.smart-scroll').length > 0) { // check if element exists
-    var last_scroll_top = 40;
-    $(window).on('scroll', function() {
-        scroll_top = $(this).scrollTop();
-        if(scroll_top < last_scroll_top) {
-            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-        }
-        else {
-            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-        }
-        last_scroll_top = scroll_top;
-    });
-}
 
 
 
-// $(function() {
-//   var scroll = $(document).scrollTop();
-//   var navHeight = $('.navigat').outerHeight();
-//   $(window).scroll(function() {
-//       var scrolled = $(document).scrollTop();
-//       if(scrolled > 20) {
-//           $('nav').addClass('nav-border');
-//       } else {
-//           $('nav').removeClass('nav-border');
-//       }
-//       if(scrolled > navHeight) {
-//           $('nav').addClass('animate');
-//           console.log('scrolling');
-//       } else {
-//           $('nav').removeClass('animate');
-//       }
-//       scroll = $(document).scrollTop();
-//   })
-// })
+$(function() {
+  var scroll = $(document).scrollTop();
+  var navHeight = $('vav').outerHeight();
+  $(window).scroll(function() {
+      var scrolled = $(document).scrollTop();
+      if(scrolled > 20) {
+          $('nav').addClass('nav-border');
+      } else {
+          $('nav').removeClass('nav-border');
+      }
+    //   if(scrolled > navHeight) {
+    //       $('nav').addClass('animate');
+    //       console.log('scrolling');
+    //   } else {
+    //       $('nav').removeClass('animate');
+    //   }
+      scroll = $(document).scrollTop();
+  })
+})
+
+
 // (function($) {          
 //   $(document).ready(function(){                    
 //       $(window).scroll(function(){                          
