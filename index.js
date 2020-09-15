67c949495b34aac8ea7c06adfda4c8a2b6894211
Jorge C <jorgec@Jorges-MacBook-Pro-2.local>
2020-09-14 21:37:28 -0700

    add image ani

--- index.js
+++ index.js
@@ -54,3 +54,98 @@ var logoAni = gsap.from('.logo', {duration: 2.5, opacity: 0, scale:0.1, ease: 'b
 
 //   gsap.registerPlugin(ScrollTrigger);
 
+
+
+
+
+
+
+
+
+
+
+
+/// starting
+let btn = document.querySelectorAll("button");
+// const next = document.querySelector('.next');
+let img = document.querySelector('.firstPic');
+let btnArr = Array.from(btn);
+let current = [];
+
+// let imges = [
+//   {'./images/mike8.JPG',
+//   './images/mike3.JPG'
+//   }
+// ];
+const reviews = {
+  image1:[
+    './images/mike2.JPG',
+    './images/mike3.JPG',
+    './images/mike4.JPG']
+  }
+    
+
+
+let currentItem = 0;
+btnArr.map((el) => {
+   el.addEventListener('click', function() {
+     if(hasClass(el, className = 'first')) {
+      console.log('Yey!')
+        const item = reviews.image1[currentItem];
+        img.src = item;
+        const next = document.querySelector('.next');
+        next.addEventListener('click', function() {
+          currentItem++;
+          console.log('clicking next');
+        if (currentItem > reviews.image1.length - 1) {
+            currentItem = 0;
+          }
+          showPerson(currentItem);
+        });
+     } else if(hasClass(el, className = 'second')) {
+       console.log('I am second');
+     }
+  });
+});
+
+
+
+  function showPerson(person) {
+    const item = reviews.image1[person];
+    img.src = item;
+    return item;
+   
+  }
+
+// let num = 0;
+// function showImage(person) {
+//   num++;
+//   const item = img1[person];
+  
+//   console.log(item);
+// }
+
+
+
+// showImage(img1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// function that will find true or false
+function hasClass(element, className) {
+  return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+}
+
+
