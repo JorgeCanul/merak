f1bd78a1cda8d0c923a9f1f4bd5941e5450a4016
Jorge C <jorgec@Jorges-MacBook-Pro-2.local>
2020-09-21 04:49:45 -0700

    work on imagePopout

--- index.js
+++ index.js
@@ -54,278 +54,133 @@ var logoAni = gsap.from('.logo', {duration: 2.5, opacity: 0, scale:0.1, ease: 'b
 
 //   gsap.registerPlugin(ScrollTrigger);
 
-
-
-/// starting
-let btn = document.querySelectorAll("button");
-const next1 = document.querySelector('.next1');
-const next2 = document.querySelector('.next2');
-const next3 = document.querySelector('.next3');
-const next4 = document.querySelector('.next4');
-const next5 = document.querySelector('.next5');
-const next6 = document.querySelector('.next6');
-const next7 = document.querySelector('.next7');
-const next8 = document.querySelector('.next8');
-
-// let btnClose1 = document.querySelector('.close1');
-let btnArr = Array.from(btn);
-
-let img1 = document.querySelector('.firstPic');
-let img2 = document.querySelector('.secondPic');
-let img3 = document.querySelector('.thirdPic');
-let img4 = document.querySelector('.fourthPic');
-let img5 = document.querySelector('.fivePic');
-let img6 = document.querySelector('.sixPic');
-let img7 = document.querySelector('.sevenPic');
-let img8 = document.querySelector('.eigthPic');
-
-
-const pictues1 = {
-  image1:[
-    './images/mike2.JPG', 
-    './images/mike3.JPG',
-    './images/mike4.JPG']
+///////////////////////////////////////////////////////
+const dataController = (function() {
+  function Image(id, image) {
+    this.id = id;
+    this.image = image;
   }
 
-  const pictues2 = {
-    image2:[
-      './images/mike10.JPG'
-      ]
-  }
-
-
-  const pictues3 = {
-    image3:[
-      './images/mike4.JPG'
-      ]
-  }
-
-  const pictues4 = {
-    image4:[
-      './images/mike5.JPG'
-      ]
-  }
-
-  const pictues5 = {
-    image5:[
-      './images/mike6.JPG'
-     ]
-  }
-
-  const pictues6 = {
-    image6:[
-      './images/mike7.JPG'
-      ]
-  }
-
-  const pictues7 = {
-    image7:[
-      './images/mike8.JPG'
-      ]
-  }
-      
-  const pictues8 = {
-    image8:[
-      './images/mike12.JPG'
-      ]
-  }
-
- 
-    
-let currentItem = 0;
-
-const item1 = pictues1.image1[currentItem];
-const item2 = pictues2.image2[currentItem];
-const item3 = pictues3.image3[currentItem];
-const item4 = pictues4.image4[currentItem];
-const item5 = pictues5.image5[currentItem];
-const item6 = pictues6.image6[currentItem];
-const item7 = pictues7.image7[currentItem];
-const item8 = pictues8.image8[currentItem];
-
-btnArr.map((el) => {
-   el.addEventListener('click', function() {
-     if(hasClass(el, className = 'first')) {
-        img1.src = item1;
-     } else if(hasClass(el, className = 'second')) {
-       img2.src = item2;
-     } else if (hasClass(el, className = 'three')) {
-      img3.src = item3;
-     } else if (hasClass(el, className = 'four')) {
-      img4.src = item4;
-     } else if (hasClass(el , className = 'five')) {
-      img5.src = item5;
-     } else if (hasClass(el , className = 'six')) {
-      img6.src = item6;
-     } else if (hasClass(el , className = 'seven')) {
-      img7.src = item7;
-     } else if (hasClass(el, className = 'eight')) {
-      img8.src = item8;
-     }
-  });
-});
-
-//// 1 ////
-next1.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img1, 'first')) {
-    if (currentItem > pictues1.image1.length - 1) {
-      currentItem = 0;
-    }
-    showItem1(currentItem);
-  } 
-});
-
-//// 2 ////
-next2.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img2, 'second')) {
-    if (currentItem > pictues2.image2.length - 1) {
-      currentItem = 0;
-    }
-    showItem2(currentItem)
-  } 
-})
-
-//// 3 ////
-next3.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img3, 'three')) {
-    if (currentItem > pictues3.image3.length - 1) {
-      currentItem = 0;
-    }
-    showItem3(currentItem)
-  } 
-})
-
-//// 4 ////
-next4.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img4, 'four')) {
-    if (currentItem > pictues4.image4.length - 1) {
-      currentItem = 0;
-    }
-    showItem4(currentItem)
-  } 
-})
-
-//// 5 ////
-next5.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img5, 'five')) {
-    if (currentItem > pictues5.image5.length - 1) {
-      currentItem = 0;
-    }
-    showItem5(currentItem)
-  } 
-})
-
-
-//// 6 ////
-next6.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img6, 'six')) {
-    if (currentItem > pictues6.image6.length - 1) {
-      currentItem = 0;
-    }
-    showItem6(currentItem)
-  } 
-})
-
-//// 7 ////
-next7.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img7, 'seven')) {
-    if (currentItem > pictues7.image7.length - 1) {
-      currentItem = 0;
-    }
-    showItem7(currentItem)
-  } 
-})
-
-//// 8 ////
-next8.addEventListener('click', function() {
-  currentItem++;
-  if(hasClass(img8, 'eight')) {
-    if (currentItem > pictues8.image8.length - 1) {
-      currentItem = 0;
-    }
-    showItem8(currentItem)
-  } 
-})
-
-////// 1 //////////
-  function showItem1(items) {
-    let item1 = pictues1.image1[items];
-    if(hasClass(img1, 'first') ) {
-      img1.src = item1;
-    } 
-  }
-
-  ////// 2 //////////
-  function showItem2(items) {
-    let item2 = pictues2.image2[items];
-     if (hasClass(img2, 'second') ) {
-      img2.src = item2;
-    }
-  }
-
-  ////// 3 //////////
-  function showItem3(items) {
-    let item3 = pictues3.image3[items];
-     if (hasClass(img3, 'three') ) {
-      img3.src = item3;
-    }
-  }
-
-  ////// 4 //////////
-  function showItem4(items) {
-    let item4 = pictues4.image4[items];
-     if (hasClass(img4, 'four') ) {
-      img4.src = item4;
-    }
-  }
-   ////// 5 //////////
-   function showItem5(items) {
-    let item5 = pictues5.image5[items];
-     if (hasClass(img5, 'five') ) {
-      img5.src = item5;
-    }
-  }
-  ////// 6 //////////
-  function showItem6(items) {
-    let item6 = pictues6.image6[items];
-     if (hasClass(img6, 'six') ) {
-      img6.src = item6;
-    }
-  }
-
-  ////// 7 //////////
-  function showItem7(items) {
-    let item7 = pictues7.image7[items];
-     if (hasClass(img7, 'seven') ) {
-      img7.src = item7;
-    }
-  }
-
-  ////// 8 //////////
-  function showItem8(items) {
-    let item8 = pictues8.image8[items];
-     if (hasClass(img8, 'eight') ) {
-      img8.src = item8;
+  let data = {
+    idOne:{
+      img1: './images/mike2.JPG',
+    },
+    idTwo: {
+      img2: './images/mike10.JPG'
     }
   }
 
 
-function hasClass(element, className) {
-  return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
-}
+  return {
+    getItem: function(id) {
+      let img;
+      if(id === 'first') {
+        img = data.idOne.img1;
+      } else if (id === 'second') {
+        img = data.idTwo.img2;
+      }
+      // data.allItems.push(newItem);
+      return img;
+    },
 
+    getItems: function() {
+      return {
+        imag1: data.idOne.img1,
+        imag2: data.idTwo.img2
+      }
+    },
 
-////// Close btn //////
-// btnClose1.addEventListener('click', function() {
-//   if(hasClass(img1, 'first')) {
-//     img1.src = item1;
-//     console.log('CLosed one');
-//   }
-// });
+    testing: function() {
+      return data;
+    }
+  }
+})();
+
+///////////////////////////////////////////////////////
+const itemController = (function() {
+  let DomStrings = {
+    // generalItem: '.first',
+    container: '.popOut'
+  }
+
+  return {
+    addItemUi: function(obj) {
+      let element, html, newHtml;
+      element = DomStrings.container;
+      html = `<div class="modal fade" id="first" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"aria-hidden="true">\n\
+      <div class="modal-dialog cascading-modal" role="document">\n\
+        <!--Content-->\n\
+        <div class="modal-content">\n\
+          <!--Header-->\n\
+          <div class="modal-header light-blue darken-3 white-text">\n\
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\n\
+          </div>\n\
+          <!--Body-->\n\
+          <div class="modal-body mb-0 text-center">\n\
+                 <img src="${obj.imag1}"  class="card-img-top firstPic first" alt="...">\n\
+             <div class="mt-2" aria-label="Page navigation example">\n\
+                 <ul class="pagination center">\n\
+                   <li class="page-item">\n\
+                     <a class="page-link prev" aria-label="Previous">\n\
+                       <span aria-hidden="true">&laquo;</span>\n\
+                     </a>\n\
+                   </li>\n\
+                   <li class="page-item">  \n\
+                     <a class="page-link next1"  aria-label="Next">\n\
+                       <span aria-hidden="true">&raquo;</span>\n\
+                     </a>\n\
+                   </li>\n\
+                 </ul>\n\
+               </div>\n\
+                <!-- <hr> -->\n\
+                <div class="survey-footer clearfix">\n\
+                    <!-- <i class="fa fa-paper-plane ml-1"></i> -->\n\
+                </div>\n\
+          </div>\n\
+        </div>\n\
+      </div>\n\
+      </div>`;
+      document.querySelector(element).insertAdjacentHTML('beforeend', html);
+    },
+
+    getDOMPublic: function() {
+      return DomStrings;
+    }
+  }
+
+})();
+
+///////////////////////////////////////////////////////
+const mainController = (function(dataCtrl, itemCtrl) {
+
+  function addEventListeners() {
+   let btn =  document.querySelectorAll('button');
+   let arrBtn = Array.from(btn);
+   arrBtn.forEach(el => {
+    el.addEventListener('click', function(e) {
+      let id;
+      if(e.path[2].classList[0] === 'first') {
+        id = 'first';
+        dataCtrl.getItem(id);
+      } else if(e.path[2].classList[0] === 'second') {
+        id = 'second';
+        dataCtrl.getItem(id);
+      }
+      addItem();
+    })
+   });
+   
+  }
+
+  function addItem() {
+    let images = dataCtrl.getItems();
+    itemCtrl.addItemUi(images);
+
+  }
+  return {
+    init: function() {
+      addEventListeners();
+    }
+  }
+})(dataController, itemController);
+mainController.init();
